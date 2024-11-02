0c6bc4419ca1e6b90026b830de8021a654db89f4
STkangyh <yhkang2003@gmail.com>
2024-11-02 19:19:31 +0900

    route 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./routes/home";
+import Calendar from "./routes/calendar";
 
 export default function App() {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <Home />,
+    },
+    {
+      path: "/calendar",
+      element: <Calendar />,
     },
   ]);
   return (
