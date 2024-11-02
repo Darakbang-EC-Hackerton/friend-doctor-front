9593756ac8627daefe99d70a75cc09c57f3ed55b
STkangyh <yhkang2003@gmail.com>
2024-11-03 07:01:30 +0900

    Fix: style 수정

--- src/routes/calendar.tsx
+++ src/routes/calendar.tsx
@@ -3,7 +3,7 @@ import CalendarComponent from "../components/calendarCom";
 
 export default function Calendar() {
   return (
-    <div>
+    <div className="flex flex-col items-center bg-[#f1f1f1] px-4 pt-6 pb-10 space-y-6">
       <CalendarComponent />
       <BottomNavbar />
     </div>
--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -4,7 +4,7 @@ import TodayPoem from "../components/todayPoem";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center p-4 space-y-6">
+    <div className="flex flex-col items-center bg-[#f1f1f1] px-4 pt-6 pb-24 space-y-6">
       <TodayEmoji />
       <TodayPoem />
       <BottomNavbar />
